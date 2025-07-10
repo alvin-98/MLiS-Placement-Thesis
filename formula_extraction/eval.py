e2d0eac2885ab04354b67297baae6938abb2e85e
tylaar1 <151372152+tylaar1@users.noreply.github.com>
2025-07-10 12:38:02 +0100

    remove duplicate lines of code

--- formula_extraction/eval.py
+++ formula_extraction/eval.py
@@ -2,19 +2,6 @@
 import pandas as pd
 import numpy as np
 from input_data import input_values
-input_values = [
-    # Inputs for compute_noise_charge
-    [
-        {'qc': 0.5, 'weight': 10, 'day_night': 'day'},
-        {'qc': 1.0, 'weight': 15, 'day_night': 'night'}
-    ],
-
-    # Inputs for compute_park_charge_west
-    [
-        {'park_type': 'Wide Remote', 'time_min': 180},
-        {'park_type': 'Narrow Remote', 'time_min': 3000}
-    ]
-]
 df=pd.read_csv('llmdata.csv')
 for i in range(df.shape[0]):
     print(df)
